19333a9e6f39112b4965a9df01906989727c515e
sitifardamaulina <80209705+sitifardamaulina@users.noreply.github.com>
2022-01-04 18:56:39 +0700

    Add files via upload

---
+++ Kuis.java
@@ -0,0 +1,121 @@
+
+package Tugas13;
+
+import java.awt.Color;
+import java.awt.GridLayout;
+import javax.swing.*;
+import java.awt.event.*;
+
+public class Kuis extends JFrame implements MouseListener, ActionListener {
+    JTextField firstValue = new JTextField();
+    JTextField secondValue = new JTextField();
+    JTextField userInput = new JTextField();
+    JLabel resultText = new JLabel();
+    JButton checkButton = new JButton("CHECK");
+
+    int randomFirstValue = getRandomNumber(1, 10);
+    int randomSecondValue = getRandomNumber(1, 20);
+    
+    Kuis() {
+        super("Kuis");
+        setSize(600, 400);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+        setLocationRelativeTo(null);
+
+     
+        userInput.addMouseListener(this);
+        checkButton.addMouseListener(this);
+        checkButton.addActionListener(this);
+        userInput.addKeyListener(new InputToIntegerOnly(userInput));
+
+       
+        JPanel guessBoardPanel = new JPanel();
+        guessBoardPanel.setLayout(new GridLayout(1, 3));
+        guessBoardPanel.add(firstValue);
+        guessBoardPanel.add(secondValue);
+        guessBoardPanel.add(userInput);
+
+        this.setLayout(new GridLayout(3, 1));
+        this.add(guessBoardPanel);
+        this.add(checkButton);
+        this.add(resultText);
+
+    
+        firstValue.setText(Integer.toString(randomFirstValue));
+        secondValue.setText(Integer.toString(randomSecondValue));
+        firstValue.setEditable(false);
+        secondValue.setEditable(false);
+    }
+    
+    public int getRandomNumber(int min, int max) {
+        return (int) ((Math.random() * (max - min)) + min);
+    }
+
+    public class InputToIntegerOnly extends KeyAdapter {
+
+        JTextField input;
+
+        InputToIntegerOnly(JTextField input) {
+            this.input = input;
+        }
+
+        @Override
+        public void keyPressed(KeyEvent ke) {
+            if (!"".equals(input.getText())) {
+                if (ke.getKeyChar() >= '0' && ke.getKeyChar() <= '9' || ke.getKeyCode() == 8) {
+                    input.setEditable(true);
+                } else {
+                    input.setEditable(false);
+                }
+            }
+
+        }
+    }
+@Override
+    public void actionPerformed(ActionEvent ae) {
+        int userInputValue = Integer.parseInt(userInput.getText());
+
+        if (ae.getSource() == checkButton) {
+            if (randomFirstValue + randomSecondValue == userInputValue) {
+                resultText.setText("Selamat, jawaban anda benar !!");
+                getContentPane().setBackground(Color.green);
+            } else {
+                resultText.setText("Maaf, jawaban anda salah !!");
+                getContentPane().setBackground(Color.red);
+            }
+
+        }
+
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    
+    public static void main(String[] args) {
+        Kuis kalkulator = new Kuis();
+    }
+
+}
